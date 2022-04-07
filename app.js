8b68d178194134b01a3774b51d6917f69e0b24d7
Kitt Cheung <kitt.cheung@se.experis.com>
2022-04-07 11:11:47 +0200

    added images and try to add pikes names to FE

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
+const bikePrice = document.getElementById("bike-price");
+const bikeName = document.getElementById("bike-name");
+let list = document.getElementById("myList");
+
+let arr = ["monster", "streetfighter", "sportpro"]
+
+arr.forEach((item) =>{
+    let div = document.createElement("div");
+    div.innerText = item;
+    list.appendChild(div);
+})
+
 async function getBikesAsync() {
     const response = await fetch("https://localhost:7211/api/");
     return await response.json();
-  }
+}
+
