8f053a519fc7fc922ff07634e0a66b1848f0cb97
Kitt Cheung <kitt.cheung@se.experis.com>
2022-04-07 16:06:30 +0200

    added images and try to add pikes names to FE

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const bikePrice = document.getElementById("bike-price");
 const bikeName = document.getElementById("bike-name");
 let list = document.getElementById("myList");
 
-let arr = ["monster", "streetfighter", "sportpro"]
+let arr = [{name:"monster", price: 22222, image:"/images/MY21-Monster-Plus-Red-Model-Blocks-630x390-01.jpg"}, {name:"streetfighter", price:33333, image:"images/MY21-Monster-Plus-Red-Model-Blocks-630x390-01.jpg"}, {name:"sportpro", price:44444, image:"images/MY21-Monster-Plus-Red-Model-Blocks-630x390-01.jpg"}]
 
 arr.forEach((item) =>{
     let div = document.createElement("div");
-    div.innerText = item;
+    div.className="col-md-4 justify-content-around";
+    let p1 = document.createElement("p");
+    let p2 = document.createElement("p");
+    let pImage = document.createElement("div")
+    p1.innerText = item.name;
+    p2.innerText = item.price;
+    div.src = item.image;
     list.appendChild(div);
+    div.appendChild(p1);
+    div.appendChild(p2);
+    div.appendChild(pImage);
+    document.getElementById("myList").appendChild(div)
 })
 
 async function getBikesAsync() {
