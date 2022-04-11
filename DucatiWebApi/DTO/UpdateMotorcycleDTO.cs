ef067ea771a891a1d30cc9f6ba2effd952438156
Kitt Cheung <kitt.cheung@live.se>
2022-04-11 12:31:26 +0200

    adding BE

---
+++ DucatiWebApi/DTO/UpdateMotorcycleDTO.cs
@@ -0,0 +1,13 @@
+﻿#nullable disable
+namespace DucatiWebApi.DTO
+{
+    public class UpdateMotorcycleDTO
+    {
+
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Year { get; set; }
+        public string ImagePath { get; set; }
+
+    }
+}
