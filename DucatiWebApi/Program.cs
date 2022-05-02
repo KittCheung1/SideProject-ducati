c8a16820d540c8e8f6b03f3944f6057e919dc1a3
Kitt Cheung <kitt.cheung@se.experis.com>
2022-05-02 16:37:17 +0200

    Feat: FE info of each bike

--- DucatiWebApi/Program.cs
+++ DucatiWebApi/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: AllowMySpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://127.0.0.1:5500/FE-ducati").AllowAnyMethod().AllowAnyHeader();
+                          policy.WithOrigins("http://127.0.0.1:5500").AllowAnyMethod().AllowAnyHeader();
 
                       });
 });
