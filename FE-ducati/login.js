45d19536e491775c261310594e4eada0041d6b5b
Kitt Cheung <kitt.cheung@live.se>
2022-05-11 17:01:27 +0200

    FEAT: login sign up feature FE

--- FE-ducati/login.js
+++ FE-ducati/login.js
@@ -1,2 +1,17 @@
 const username = document.getElementById("username").value
 const password = document.getElementById("password").value
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+    
+    document.querySelector("#linkCreateAccount").addEventListener("click", () => {
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+    });
+    document.querySelector("#linkLogin").addEventListener("click", () => {
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+    });
+
+});
