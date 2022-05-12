acd3fde18ef6c85a03c6fd7227eb1eae164e4021
Kitt Cheung <kitt.cheung@live.se>
2022-05-12 14:09:31 +0200

    FEAT: login sign up feature FE

--- FE-ducati/login.js
+++ FE-ducati/login.js
@@ -1,17 +1,29 @@
 const username = document.getElementById("username").value
 const password = document.getElementById("password").value
 
+// making one of the forms hidden when clicking on a link
 document.addEventListener("DOMContentLoaded", ()=>{
     const loginForm = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createAccount");
     
-    document.querySelector("#linkCreateAccount").addEventListener("click", () => {
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
         loginForm.classList.add("form--hidden");
         createAccountForm.classList.remove("form--hidden");
     });
-    document.querySelector("#linkLogin").addEventListener("click", () => {
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
         loginForm.classList.remove("form--hidden");
         createAccountForm.classList.add("form--hidden");
     });
 
 });
+
+// formElement = loginForm or createAccountForm
+// type = success or error type of message
+// message = the actual message
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+}
