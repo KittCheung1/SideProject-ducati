08303c212dd90726b6745f0c1ef261893924bac6
Kitt Cheung <kitt.cheung@se.experis.com>
2022-05-13 13:31:11 +0200

    User controller

--- DucatiWebApi/Controllers/UserController.cs
+++ DucatiWebApi/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using DucatiWebApi.Data;
+using DucatiWebApi.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DucatiWebApi.Controllers
@@ -16,7 +17,7 @@ namespace DucatiWebApi.Controllers
         }
 
         //GET: api/users = gets user with same username
-        [HttpGet("/api/users/{username}")]
+        [HttpGet("/api/users/")]
         public async Task<ActionResult<UserController>> GetUser(string username)
         {
             var user = await _context.Users.FindAsync(username);
@@ -25,6 +26,15 @@ namespace DucatiWebApi.Controllers
                 return NotFound();
             }
             return Ok(user);
+        }
+
+        [HttpPost("/api/users/{id}")]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
         }
     }
 }
